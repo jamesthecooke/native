71c804d738b1f80d8ca609fe3b25e498cc11194f
jamesthecooke <jamesjcooke03@gmail.com>
2024-02-12 16:18:03 +0000

    started to add a basic layout for harvest hub project page

--- AwesomeProject/app/screens/HarvestHub.js
+++ AwesomeProject/app/screens/HarvestHub.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { Text } from "react-native";
+import { ImageBackground, Text, StyleSheet, View, Image } from "react-native";
 
 function HarvestHub(props) {
-  return <Text>Project here</Text>;
+  return (
+    <ImageBackground style={styles.background}>
+      <View style={styles.projectImgContainer}>
+        <Image
+          source={require("../assets/background.jpg")}
+          style={styles.projectImg}
+        ></Image>
+      </View>
+    </ImageBackground>
+  );
 }
 
 export default HarvestHub;
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex",
+    backgroundColor: "black",
+  },
+
+  projectImgContainer: {
+    flex: 0.35,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+
+  projectImg: {
+    flex: 1,
+    width: "85%",
+    height: "100%",
+    borderRadius: 20,
+  },
+});
 // comment added
