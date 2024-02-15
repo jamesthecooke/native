f6d6e574cb1ce75430d0534ccabe535fb72b03b9
jamesthecooke <jamesjcooke03@gmail.com>
2024-02-15 16:02:24 +0000

    updated the description for pong

--- AwesomeProject/app/screens/projects/Pong.js
+++ AwesomeProject/app/screens/projects/Pong.js
@@ -18,9 +18,14 @@ function Pong(props) {
       </ImageBackground>
       <View style={styles.pageBody}>
         <Text style={styles.descriptionText}>
-          Harvest Hub my awsome 4 week project that was worked on in a team of
-          6. this is place holder text and will be much mich better formatted
-          than this teheeh
+          Pong was the first little game I developed. I used the language Lua
+          and the framework _Love2D_ to create it. It was a fun little project
+          and I learned a lot from it. I learned about the game loop, collision
+          detection, and how to handle user input. I also learned about the
+          importance of game design and how to make a game fun to play. I am
+          proud of this project because it was the first game I ever made and it
+          was a great learning experience. I hope to make more games in the
+          future and continue to improve my skills.
         </Text>
       </View>
     </ImageBackground>
